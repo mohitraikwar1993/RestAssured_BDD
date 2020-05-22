f4d5ba06a3f5c127c5000e9b9710f3d83c6088ed
mohitraikwar1993 <raikmohit@gmail.com>
2020-05-22 17:19:43 +0530

    commit

--- RestAssured_BDD/src/test/java/commonUtilities/DataProviders.java
+++ RestAssured_BDD/src/test/java/commonUtilities/DataProviders.java
@@ -7,7 +7,7 @@ public class DataProviders
 	@DataProvider(name="testdata")
 	public static Object[][] dataset()
 	{
-	return new Object[][] {{"shatvika","111"},{"Mohini","111"},{"kaveri","111"}};
+	return new Object[][] {{"shatvika","111"},{"Mohini","11"},{"kaveri","111"}};
 	}
 
 
